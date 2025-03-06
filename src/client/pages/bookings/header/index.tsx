1e5507e44d7e29055adb41ad6c7eed2a50799142
Omar <omerx9988@gmail.com>
2025-03-06 06:19:46 +0200

    feat: implement manual booking creation button in bookings header

--- src/client/pages/bookings/header/index.tsx
+++ src/client/pages/bookings/header/index.tsx
@@ -3,26 +3,42 @@
  */
 import { __ } from '@wordpress/i18n';
 import { PlusOutlined } from '@ant-design/icons';
+import { useState } from '@wordpress/element';
 
 /**
  * External dependencies
  */
 import { Button, Flex, Popover, Typography } from 'antd';
 
+interface BookingsHeaderProps {
+	handleOpen: (state: boolean) => void;
+}
 /**
  * Main Bookings Component.
  */
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
-const BookingsHeader: React.FC = () => {
+const BookingsHeader: React.FC<BookingsHeaderProps> = ({ handleOpen }) => {
+	const [visible, setVisible] = useState<boolean>(false);
+
 	return (
 		<Flex justify="space-between" align="center">
 			<Title level={3}>{__('Bookings', 'quillbooking')}</Title>
 			<Popover
 				content={
-					<Text>{__('Create Booking Manually', 'quillbooking')}</Text>
+					<Button
+						type="link"
+						onClick={() => {
+							handleOpen(true);
+							setVisible(false);
+						}}
+					>
+						{__('Create Booking Manually', 'quillbooking')}
+					</Button>
 				}
 				trigger={'click'}
+				open={visible}
+				onOpenChange={(visible) => setVisible(visible)}
 			>
 				<Button>
 					<PlusOutlined />

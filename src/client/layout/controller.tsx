09c82d0aa04650c7feb0df607dc271993a62b471
emy185 <emyy729@gmail.com>
2025-03-23 10:00:53 +0200

    update 1

--- src/client/layout/controller.tsx
+++ src/client/layout/controller.tsx
@@ -26,6 +26,17 @@ import CreateEvent from '../pages/create-event';
 import Bookings from '../pages/bookings';
 import BookingDetails from '../pages/booking-details';
 import Event from '../pages/event';
+import Settings from '../pages/settings';
+import Help from '../pages/help';
+import Logout from '../pages/logout';
+import AvailaibilityIcon from '../../components/icons/availability-icon';
+import BookingIcon from '../../components/icons/booking-icon';
+import CalendarsIcon from '../../components/icons/calendars-icon';
+import HelpIcon from '../../components/icons/help-icon';
+import HomeIcon from '../../components/icons/home-icon';
+import SettingsIcon from '../../components/icons/settings-icon';
+import LogoutIcon from '../../components/icons/logout-icon';
+
 
 export const Controller = ({ page }) => {
 	useEffect(() => {
@@ -46,13 +57,23 @@ export const Controller = ({ page }) => {
 registerAdminPage('home', {
 	path: '/',
 	component: () => <Home />,
-	label: __('Home', 'quillbooking'),
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<HomeIcon/>
+			<span>{__('Home', 'quillbooking')}</span>
+		</span>
+	),
 });
 
 registerAdminPage('calendars', {
 	path: 'calendars',
 	component: () => <Calendars />,
-	label: __('Calendars', 'quillbooking'),
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<CalendarsIcon/>
+			<span>{__('Calendars', 'quillbooking')}</span>
+		</span>
+	),
 });
 
 registerAdminPage('calendar', {
@@ -62,11 +83,63 @@ registerAdminPage('calendar', {
 	hidden: true,
 });
 
+registerAdminPage('bookings', {
+	path: 'bookings',
+	component: () => <Bookings />,
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<BookingIcon />
+			<span>{__('Bookings', 'quillbooking')}</span>
+		</span>
+	),
+})
+
 registerAdminPage('event', {
 	path: 'calendars/:id/events/:eventId/:tab?',
 	component: () => <Event />,
 	label: __('Event', 'quillbooking'),
 	hidden: true,
+});
+
+registerAdminPage('availability', {
+	path: 'availability',
+	component: () => <Availability />,
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<AvailaibilityIcon/>
+			<span>{__('Availability', 'quillbooking')}</span>
+		</span>
+	),
+});
+
+registerAdminPage('settings', {
+	path: 'settings',
+	component: () => <Settings />,
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<SettingsIcon/>
+			<span>{__('Settings', 'quillbooking')}</span>
+		</span>),
+});
+
+registerAdminPage('help', {
+	path: 'help',
+	component: () => <Help />,
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<HelpIcon/>
+			<span>{__('Help and Support', 'quillbooking')}</span>
+		</span>),
+});
+
+registerAdminPage('logout', {
+	path: 'logout',
+	component: () => <Logout />,
+	label: (
+		<span style={{ display: "flex", alignItems: "center", color: "#292D32", fontWeight: "500", fontSize: "16px" }}>
+			<LogoutIcon/>
+			<span>{__('Logout', 'quillbooking')}</span>
+		</span>),
 });
 
 registerAdminPage('availability', {
@@ -79,19 +152,16 @@ registerAdminPage('availability/:id', {
 	path: 'availability/:id',
 	component: () => <AvailabilityDetails />,
 	label: __('Availability Details', 'quillbooking'),
+	hidden: true
 });
 
 registerAdminPage('create-event', {
 	path: 'calendars/:id/create-event/:type',
+	hidden: true,
 	component: () => <CreateEvent />,
 	label: __('Create Event', 'quillbooking'),
 });
 
-registerAdminPage('bookings', {
-	path: 'bookings',
-	component: () => <Bookings />,
-	label: __('Bookings', 'quillbooking'),
-})
 
 registerAdminPage('booking-details', {
 	path: 'bookings/:id',
---
+++ src/client/pages/help/index.tsx
@@ -0,0 +1,10 @@
+const Help: React.FC = () => {
+
+    return (
+        <div className="home">
+            <h1 className="text-red-50">Help</h1>
+        </div>
+    );
+};
+
+export default Help;

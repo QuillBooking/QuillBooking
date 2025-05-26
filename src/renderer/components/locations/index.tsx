c193647b28b9f543728d56d85038974b0fea7b1f
omar <omerx9988@gmail.com>
2025-05-26 10:33:05 +0300

    refactor: create Locations component for dynamic location fields rendering

---
+++ src/renderer/components/locations/index.tsx
@@ -0,0 +1,88 @@
+import { Form, Input, Radio } from 'antd';
+import DynamicLocationFields from '../dynamic-location-field';
+
+/**
+ * Renders dynamic fields for a selected location type
+ */
+interface Field {
+	label: string;
+	desc: string;
+	type: string;
+	required?: boolean;
+}
+
+interface Option {
+	value: string;
+	label: string;
+	fields?: Record<string, Field>;
+}
+
+interface LocationsProps {
+	locationFields: {
+		label: string;
+		options: Option[];
+	};
+}
+
+const Locations = ({ locationFields }: LocationsProps) => {
+	console.log('Locations', locationFields);
+	return (
+		<>
+			{locationFields.options.length > 1 ? (
+				<>
+					<Form.Item
+						key="location"
+						name="location"
+						label={
+							<div className="form-label">
+								<p>
+									{locationFields.label}
+									<span className="required">*</span>
+								</p>
+							</div>
+						}
+					>
+						<Radio.Group>
+							{locationFields.options.map((option) => (
+								<Radio key={option.value} value={option.value}>
+									{option.label}
+								</Radio>
+							))}
+						</Radio.Group>
+					</Form.Item>
+					<DynamicLocationFields locations={locationFields.options} />
+				</>
+			) : (
+				locationFields.options[0].fields &&
+				Object.entries(locationFields.options[0].fields).map(
+					([fieldKey, field]) => {
+						const typedField = field as Field & {
+							placeholder?: string;
+						};
+						return (
+							<Form.Item
+								key={fieldKey}
+								name={['field', fieldKey]}
+								label={
+									<div className="form-label">
+										<p>
+											{typedField.label}
+											<span className="required">*</span>
+										</p>
+									</div>
+								}
+							>
+								<Input
+									placeholder={typedField.placeholder}
+									type={typedField.type}
+								/>
+							</Form.Item>
+						);
+					}
+				)
+			)}
+		</>
+	);
+};
+
+export default Locations;

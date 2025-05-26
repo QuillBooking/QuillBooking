e5b57c72b212acc29589f7c21b369eeeaa513c07
omar <omerx9988@gmail.com>
2025-05-26 16:57:56 +0300

    fix: update form field handling for location inputs in booking components

--- src/client/pages/bookings/add-booking-modal/locations/index.tsx
+++ src/client/pages/bookings/add-booking-modal/locations/index.tsx
@@ -63,23 +63,37 @@ const Locations = ({ locationFields }: LocationsProps) => {
 							placeholder?: string;
 						};
 						return (
-							<Form.Item
-								key={fieldKey}
-								name={['field', fieldKey]}
-								label={
-									<div className="form-label">
-										<p>
-											{typedField.label}
-											<span className="required">*</span>
-										</p>
-									</div>
-								}
-							>
-								<Input
-									placeholder={typedField.placeholder}
-									type={typedField.type}
-								/>
-							</Form.Item>
+							<>
+								<Form.Item
+									key="location"
+									name="location"
+									initialValue={
+										locationFields.options[0].value
+									}
+									style={{ display: 'none' }}
+								>
+									<Input type="hidden" />
+								</Form.Item>
+								<Form.Item
+									key={fieldKey}
+									name={['field', fieldKey]}
+									label={
+										<div className="form-label">
+											<p>
+												{typedField.label}
+												<span className="required">
+													*
+												</span>
+											</p>
+										</div>
+									}
+								>
+									<Input
+										placeholder={typedField.placeholder}
+										type={typedField.type}
+									/>
+								</Form.Item>
+							</>
 						);
 					}
 				)

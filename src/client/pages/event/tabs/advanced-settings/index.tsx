b3372df8b933ef71a9d4abb96a4f97a7def1595d
omar <omerx9988@gmail.com>
2025-06-10 15:20:42 +0300

    Refactor AdvancedSettings component to use forwardRef and fix typo in filter name

--- src/client/pages/event/tabs/advanced-settings/index.tsx
+++ src/client/pages/event/tabs/advanced-settings/index.tsx
@@ -1,10 +1,23 @@
 import { AdvancedSettingsIcon, ProTab } from '@quillbooking/components';
+import { forwardRef } from '@wordpress/element';
 import { applyFilters } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
-const AdvancedSettings: React.FC = () => {
+interface EventAdvancedSettingsProps {
+	disabled: boolean;
+	setDisabled: (disabled: boolean) => void;
+}
+
+interface EventAdvancedSettingsHandle {
+	saveSettings: () => Promise<void>;
+}
+
+const AdvancedSettings = forwardRef<
+	EventAdvancedSettingsHandle,
+	EventAdvancedSettingsProps
+>(({ disabled, setDisabled }, ref) => {
 	return applyFilters(
-		'quillbooking.event.advaned_settings_tab',
+		'quillbooking.event.advanced_settings_tab',
 		<ProTab
 			title={__('Advanced Settings', 'quillbooking')}
 			description={__(
@@ -12,8 +25,9 @@ const AdvancedSettings: React.FC = () => {
 				'quillbooking'
 			)}
 			icon={<AdvancedSettingsIcon />}
-		/>
+		/>,
+		{ disabled, setDisabled, ref }
 	) as React.ReactNode;
-};
+});
 
 export default AdvancedSettings;
--- src/components/field-wrapper/index.tsx
+++ src/components/field-wrapper/index.tsx
@@ -11,8 +11,8 @@ import { Typography, Flex } from 'antd';
 const { Text } = Typography;
 
 interface FieldWrapperProps {
-	label?: string;
-	description?: string;
+	label?: string | React.ReactNode;
+	description?: string | React.ReactNode;
 	children: React.ReactNode;
 	style?: React.CSSProperties;
 	type?: 'horizontal' | 'vertical';
@@ -22,7 +22,9 @@ interface FieldWrapperProps {
 /**
  * Reusable component for descriptions.
  */
-const DescriptionText: React.FC<{ text?: string }> = ({ text }) => {
+const DescriptionText: React.FC<{ text?: string | React.ReactNode }> = ({
+	text,
+}) => {
 	if (!text) return null;
 	return <Text type="secondary">{text}</Text>;
 };
@@ -43,7 +45,7 @@ const FieldWrapper: React.FC<FieldWrapperProps> = ({
 			style={style}
 		>
 			<Flex vertical gap={5}>
-				<div className='flex items-center'>
+				<div className="flex items-center">
 					{label && <Text>{label}</Text>}
 					{label && required && <Text type="danger">*</Text>}
 				</div>

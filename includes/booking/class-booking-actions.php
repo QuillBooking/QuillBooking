a09ea1724ebbcd4ae82c57924c04c18824882f50
omar <omerx9988@gmail.com>
2025-06-12 11:03:08 +0300

    feat: include global settings in reschedule page configuration

--- includes/booking/class-booking-actions.php
+++ includes/booking/class-booking-actions.php
@@ -358,7 +358,8 @@ class Booking_Actions {
 	 * Render reschedule page
 	 */
 	protected function render_reschedule_page() {
-		$booking_id = Arr::get( $_GET, 'id', null );
+		$booking_id      = Arr::get( $_GET, 'id', null );
+		$global_settings = $this->globalSettingsClass::get_all();
 
 		if ( ! $booking_id ) {
 			wp_redirect( home_url() );
@@ -388,10 +389,11 @@ class Booking_Actions {
 
 		add_filter(
 			'quillbooking_config',
-			function ( $config ) use ( $booking, $calendar, $event ) {
-				$config['calendar'] = $calendar->toArray();
-				$config['event']    = $event->toArray();
-				$config['booking']  = $booking->toArray();
+			function ( $config ) use ( $booking, $calendar, $event, $global_settings ) {
+				$config['calendar']        = $calendar->toArray();
+				$config['event']           = $event->toArray();
+				$config['booking']         = $booking->toArray();
+				$config['global_settings'] = $global_settings;
 
 				return $config;
 			}
